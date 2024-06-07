9472d5fabc94f5d3fe7fcdbb069d79d39b04fd45
Pedro Henrique Reis Lima <pedro23reislima@gmail.com>
2024-06-07 09:20:24 -0300

    feat: Página resultado, automatizada. Os pontos aparecem de acordo com o respondido.

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -2,9 +2,42 @@ import { trocaTema, verificarTema } from "../../helpers/tema-helpers.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
+const assunto = localStorage.getItem("assunto")
 
 botaoTema.addEventListener("click", () => {
     trocaTema(body, botaoTema)
 })
 
 verificarTema(body, botaoTema)
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLocaleLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `ícone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+
+alterarAssunto()
+
+function inserirResultado() {
+    const sectionPontuação = document.querySelector(".pontuacao")
+    const divAssunto = document.querySelector(".assunto")
+    const pontos = localStorage.getItem("pontos")
+
+    sectionPontuação.innerHTML = `
+        ${divAssunto.outerHTML}
+
+        <strong>${pontos}</strong>
+
+        <p>de 10</p>
+
+        <button>Jogar novamente</button>
+    `
+}
+
+inserirResultado()
