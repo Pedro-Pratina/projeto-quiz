2323c642e28b3a1ba81e4bae6817a6453f25fe4e
Pedro Henrique Reis Lima <pedro23reislima@gmail.com>
2024-06-06 21:18:15 -0300

    fix: Arrumei o erro. Não me pergunta oque aconteceu, pq eu não sei.

--- script.js
+++ script.js
@@ -6,28 +6,3 @@ const body = document.querySelector("body")
 botaoTema.addEventListener("click", () => {
     trocaTema(body, botaoTema)
 })
-
-<<<<<<< HEAD
-verificarTema(body, botaoTema)
-=======
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
-
-function verificarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-    }
-}
-
-verificarTema(body)
->>>>>>> d619cf0d1ba1f65e57fbe0fa9a7814dcb47dd0c5
